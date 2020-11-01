cf6670af8dd003531bc16e28415ed6d7d05d9011
Azamat <azamated@gmail.com>
2020-11-01 15:34:32 +0300

    Final

--- AdminNode_GCP/gcp.tf
+++ AdminNode_GCP/gcp.tf
@@ -53,8 +53,7 @@ resource "google_compute_instance" "vm_instance1" {
       "wget && wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
       "sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
       "apt-get update && apt-get install jenkins python-boto python-pip ansible awscli -y",
-      "pip install boto",
-      "ssh-keygen -t rsa -b 4096"
+      "pip install boto"
     ]
   }
     connection {
