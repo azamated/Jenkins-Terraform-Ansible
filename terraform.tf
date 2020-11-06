cbd3f1436e93f82bf1bf6e9d2a0533c1ac3770fa
Azamat <azamated@gmail.com>
2020-11-06 21:15:01 +0300

    Minor correction

--- terraform.tf
+++ terraform.tf
@@ -6,13 +6,23 @@ terraform {
     }
   }
 }
+
+variable "aws_access_key" {
+  type        = string
+}
+variable "aws_secret_key" {
+  type        = string
+}
+
+
 ######################
 #Create aws instances#
 ######################
 provider "aws" {
   region = "us-east-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
-
 
 
 #Copy a public key to instances
