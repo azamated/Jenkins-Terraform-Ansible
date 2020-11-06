c6f6d03448ed36dbb5c187da45e2bbee2ac787de
Azamat <azamated@gmail.com>
2020-11-06 21:29:43 +0300

    Minor correction

--- terraform.tf
+++ terraform.tf
@@ -20,8 +20,8 @@ variable "aws_secret_key" {
 ######################
 provider "aws" {
   region = "us-east-2"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
 }
 
 
