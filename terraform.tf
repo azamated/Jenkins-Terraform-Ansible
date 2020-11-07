e4a29af31e9b661ce28cda70a388982c480893c1
Azamat <azamated@gmail.com>
2020-11-07 03:27:54 +0300

    Minor correction

--- terraform.tf
+++ terraform.tf
@@ -31,7 +31,7 @@ resource "aws_key_pair" "id_rsa" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDha4tKrImw/ij1qy5obpegrTFHSwuRAZky1gSGii0Fh1D3Wtu4EoPOLqrfyDy9VzwSkhVdd9iXJAw1QMFM490s+dN0nwVopArTUfC+gp+b31q3fOL6FnCcF7mU2S8giHElqTOsWlvGu+P/CgiwZlL0Xx/glK40DApJ+pb2do7DJsOjsPA+y3qiIlNGK68u3Qu0DCFBIIQ1QgAl/fCNeuFLVtE2Xg2fOxFIl3n0INSnFK4jvyLZ/24usH1nJVC2cST93WwEGLijdbuAk+w5mKPJCjHAb8MH2Jgqyv2Tsgb8al5FQCnc4lzPtraf1GCMcX02RDC3d7yFBRxrmQxFWcHvcGT3K+f+SRMssll5QJbC+G5VKNDeb8ZpTx/BJ1G6Fj5gTLPoRf83RMiY8TGdQoZig8qdvb3SAGb3KMygr5LUhx/O4B4fK1SUpnhvSZ8I8WSTtJAtBfIVJU9AXYK5Pcbm4DOvOC//1JCL8Lx2g4jnsfP91+bJqxL2/D1Y1xQAPUk= root@devops"
 }
 
-resource "aws_instance" "builder11" {
+resource "aws_instance" "builder12" {
   ami = "ami-07efac79022b86107"
   instance_type = "t2.micro"
   monitoring = true
@@ -42,7 +42,7 @@ resource "aws_instance" "builder11" {
   }
 }
 
-resource "aws_instance" "production11" {
+resource "aws_instance" "production12" {
   ami = "ami-07efac79022b86107"
   instance_type = "t2.micro"
   monitoring = true
